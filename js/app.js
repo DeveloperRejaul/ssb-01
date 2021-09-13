59c349484fca9105b623de2aa5ed34b9d81089a4
DeveloperRejaul <85983722+DeveloperRejaul@users.noreply.github.com>
2021-09-13 21:44:37 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider-aria').bxSlider();
+    $('#main-menu').meanmenu({
+        meanMenuContainer:'#mobile-menu',
+        meanScreenWidth:991,
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
